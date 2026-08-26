7032542e3d181d14e0a98cb8e34d8c452f53f480
shlok <raskarshlok07@gmail.com>
2026-08-26 11:43:43 +0530

    Intialization of A Project

---
+++ src/test/java/com/Shlok/Booking_System/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shlok.Booking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
