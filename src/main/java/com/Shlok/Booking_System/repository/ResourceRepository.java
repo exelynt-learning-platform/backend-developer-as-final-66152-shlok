eae67450450fd37592c0069ac1724c9e29724ede
shlok <raskarshlok07@gmail.com>
2026-08-27 09:54:25 +0530

    feat: implement the Repository Layer

---
+++ src/main/java/com/Shlok/Booking_System/repository/ResourceRepository.java
@@ -0,0 +1,8 @@
+package com.Shlok.Booking_System.repository;
+
+import com.Shlok.Booking_System.entity.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceRepository extends JpaRepository<Resource,Long> {
+
+}
